ea25358c4ca4cf06bce389f42a37a9878e502449
David Michalitsch <david.michalitsch@gmail.com>
2022-03-16 15:04:40 +0100

    2nd option

--- classwork/classwork.php
+++ classwork/classwork.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>Classwork PHP Day 3</title>
 </head>
 <body>
@@ -87,15 +89,23 @@ Back-End
 19
 Front-End
 etc.     -->
-<?php
+
+
+
+<div class="row">
+    <div class="col text-end">
+        <h5>Option 1</h5>
+    <?php
+    
+    // option 1 
        for($i=1; $i<101; $i++){
             // checks if $i can be divided by 5. if not, it just prints i
             if($i%5){
-               $result=$i;
+                $result=$i;
             }
             // checks if $i can be divided by 3, if true then "Back-End" will be printed
             if(!($i%3)){
-            $result="Back-End";
+                $result="Back-End";
             }
             // checks if $i can be divided by 5 but not by 3, if true then "Front-End" will be printed
             if(!($i%5) && ($i%3)){
@@ -109,7 +119,29 @@ etc.     -->
     }
 
 ?>
+    </div>
+    <div class="col text-start">
+    <h5>Option 2</h5>
+    <?php
+    // option 2 
 
+    for($i=1; $i<101; $i++){
+        if(($i%5)==0 && ($i%3)==0){
+            $result2 = "Full-Stack";
+        } 
+        elseif (!($i%5) && ($i%3)){
+            $result2 = "Front-End";
+        }
+        elseif (($i%5) && !($i%3)){
+            $result2 = "Back-End";
+        } else {
+            $result2 = $i;
+        }    
+        echo "<br>".$result2;
+    }
+    ?>
+    </div>
+</div>
 
 </body>
 </html>
